afdec3fda29526777471f422d95b5d440ef05ae5
Joseph Barra <jbarra@datto.com>
2021-01-15 06:11:55 -0500

    initial commit

---
+++ app/src/main/java/com/ciphra/android/countrylist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ciphra.android.countrylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ciphra.android.countrylist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ciphra/android/countrylist/Models/CountryModels.kt
@@ -0,0 +1,2 @@
+package com.ciphra.android.countrylist.Models
+
